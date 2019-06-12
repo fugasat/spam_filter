a9022034756b7392cc0300e9d72bb8b4056e8cc1
Satoru <>
2019-06-12 12:57:31 +0900

    出力層を調整、validationデータ追加、学習中のログを表示

--- src/model.py
+++ src/model.py
@@ -5,22 +5,30 @@ from keras.layers.recurrent import LSTM
 from keras.layers.wrappers import Bidirectional
 from keras.layers.pooling import AveragePooling1D
 from keras.models import Model
+from keras.models import Sequential
 
 
 def create(vocab_size, embedding_dim, seq_length, lstm_units):
+    """
     embedding = Embedding(input_dim=vocab_size, output_dim=embedding_dim)
     input = Input(shape=(seq_length,))
-    embed = embedding(input)
-    bilstm = Bidirectional(LSTM(lstm_units, return_sequences=True), merge_mode='concat')(embed)
-    h = Dropout(0.2)(bilstm)
+    out = embedding(input)
+    out = Bidirectional(LSTM(lstm_units, return_sequences=True), merge_mode='concat')(out)
+    out = Dropout(0.2)(out)
     #h = AveragePooling1D(pool_size=seq_length, strides=1)(h)
-    h = Reshape((seq_length * lstm_units * 2,))(h)
-    output = Dense(2, activation='softmax')(h)
+    out = Reshape((seq_length * lstm_units * 2,))(out)
+    output = Dense(2, activation='softmax')(out)
 
     model = Model(inputs=input, outputs=output)
     model.summary()
 
     model.compile(optimizer="adam", loss="categorical_crossentropy")
     #model.compile(loss="categorical_crossentropy", optimizer="sgd", metrics=["accuracy"])
-
+    """
+    model = Sequential()
+    model.add(Embedding(input_dim=vocab_size, output_dim=embedding_dim))
+    model.add(Bidirectional(LSTM(lstm_units)))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
     return model
--- src/train.py
+++ src/train.py
@@ -3,14 +3,27 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import csv
 import datetime
+import random
+from sklearn.model_selection import train_test_split
 from src import util, model
+from keras.callbacks import Callback
+
+
+# callback
+class LossHistory(Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+
+    def on_batch_end(self, batch, logs={}):
+        print(logs)
+        self.losses.append(logs.get('loss'))
 
 
 if __name__ == '__main__':
-    train_file_filter = "train_00*.txt"
+    train_file_filter = "train_*.txt"
     test_file_filter = "test_*.txt"
     embedding_dim = 100  # 単語ベクトルの次元数
-    lstm_units = 200  # LSTMの隠れ状態ベクトルの次元数
+    lstm_units = 150  # LSTMの隠れ状態ベクトルの次元数
     epoch_size = 1
     batch_size = 50
 
@@ -21,19 +34,33 @@ if __name__ == '__main__':
 
     train, test, seq_length, vocab_size = util.create_train_data(sentences_train, sentences_test)
 
+    X_train, X_test, y_train, y_test = train_test_split(train,
+                                                        labels,
+                                                        test_size=0.15,
+                                                        random_state=random.randint(0, 100))
+
     # create model
     model = model.create(
         vocab_size=vocab_size, embedding_dim=embedding_dim, seq_length=seq_length, lstm_units=lstm_units)
 
     # train param
-    model.fit(train, labels, epochs=epoch_size, batch_size=batch_size)
+    print(train)
+    print(labels)
+    #model.fit(train, labels, epochs=epoch_size, batch_size=batch_size)
+    losshist = LossHistory()
+    model.fit(X_train, y_train, verbose=2,
+              batch_size=batch_size,
+              nb_epoch=epoch_size,
+              validation_data=[X_test, y_test],
+              callbacks=[losshist])
 
     print("predict test data...")
     results = model.predict(test)
+    print(results)
     model.save('model.h5', include_optimizer=False)
 
     result_submit = util.to_result_submit(results, test_files)
-    print(result_submit)
+    #print(result_submit)
 
     now = datetime.datetime.now()
     with open("./results/result_submit_{0:%Y%m%d_%H}.csv".format(now), 'w') as f:
